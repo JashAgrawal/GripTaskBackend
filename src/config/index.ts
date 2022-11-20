db0ce09e71f82664708b43edc8d767ee9f6794be
Jash Agrawal <agrawaljash99@gmail.com>
2022-11-21 03:01:23 +0530

    fixed build errors

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const envFound = dotenv.config();
-if (envFound.error) {
-  // This error should crash whole process
-  console.log(process.env.MONGODB_URI);
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+// const envFound = dotenv.config();
+// if (envFound.error) {
+//   // This error should crash whole process
+//   console.log(process.env.MONGODB_URI);
+//   throw new Error("⚠️  Couldn't find .env file  ⚠️");
+// }
 
 export default {
   /**
