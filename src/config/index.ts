2fd0cf06a59631115b9000b69f9d26f2140b9f1c
Jash Agrawal <agrawaljash99@gmail.com>
2022-11-21 03:08:59 +0530

    fixed build errors

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-// const envFound = dotenv.config();
+const envFound = dotenv.config();
 // if (envFound.error) {
 //   // This error should crash whole process
 //   console.log(process.env.MONGODB_URI);
